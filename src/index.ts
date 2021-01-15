92b8be8161aca478892e62d0c6a1d9b9e4e14b95
manci <javiercaliescali@gmail.com>
2021-01-15 17:42:21 -0500

    221 desplegar el public folder

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-console.log('codigo de typescript');
+import Server from './server/server';
+
+const server = Server.init(3000);
+
+server.start(() => {
+  console.log('Servidor corriendo en el puerto 3000');
+});
 
